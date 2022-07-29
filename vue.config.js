e7a88af2d63253f0eabb591bd944302657f9c5c0
rayhsz <1979381950@qq.com>
2022-07-29 02:21:11 +0000

    update src/http/config.js.

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// module.exports={
+//   publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
+//   outputDir: "dist",
+//   indexPath: "index.html",
+//   filenameHashing: true,
+//   lintOnSave: process.env.NODE_ENV === "production",
+//   runtimeCompiler:false,
+//   productionSourceMap:true,
+// };
